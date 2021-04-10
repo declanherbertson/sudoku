1ede842eab5664b665f4e9f2f0aa9ff326d666f4
cerww <cerw@students.cs.ubc.ca>
2021-04-09 18:04:47 -0700

    added ability to specift files from input

--- main.pl
+++ main.pl
@@ -1,4 +1,14 @@
 :- [sudoku].
 :- [csv].
 
-solve_file(Filename) :- csv_to_matrix(Filename, Matrix), sudoku(Matrix), matrix_to_csv('out.csv', Matrix).
+
+solve_file(Filename,OutFile) :- csv_to_matrix(Filename, Matrix), sudoku(Matrix), matrix_to_csv(OutFile, Matrix).
+
+solve_sudoku:-
+	write('Enter input file name'),
+	nl,
+	read(Input),
+	write('Enter output file name'),
+	nl,
+	read(Output),
+	solve_file(Input,Output).
